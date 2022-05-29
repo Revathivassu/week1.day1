c718700d9376373daa8c493de3c96e3138e52fb6
Revathivassu <103889881+Revathivassu@users.noreply.github.com>
2022-05-28 20:31:42 -0400

    Add files via upload

--- Mobile.java
+++ Mobile.java
@@ -1,28 +1,25 @@
-package week1.day1;
+package week3.day1;
 
 public class Mobile {
-  	       // makeCall
-	       // sendMsg
+
+	public void sendMsgs() {
+		System.out.println("Send Msg");
+	}
+	public void makeCall() {
+		System.out.println("Make a call");
+	}
 	
-	     
+	public void saveContact() {
+		
+		System.out.println("Save the Contact");
+	}
 	
-	      public void makeCall () {
-	    	  System.out.println("Make the call");
-	    	  
-	      }
-	      public void sendMsg ()  {
-	    	  System.out.println("Send the Msg");
-	    	  
-	      }
-      
-	     public static void main(String[]  args) {
-	    	 System.out.println("Mobile Activity");
-	        //create  an  object 
-	    	 Mobile myMobile  = new Mobile ();
-	    	 myMobile . makeCall ();
-	    	 myMobile . sendMsg ();
-	    	 
-	     }	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+
+	}
+
 }
-	     
- 
